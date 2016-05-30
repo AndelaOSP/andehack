54a61379f4f996d67e4d4290ef3d9f80397bf041
andela-thomas <thomas.nyambati@andela.com>
2016-05-30 21:01:28 +0300

    Scaffolds angular app tests and server

---
+++ tests/client/components/home-component.spec.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+  // require the testing modules
+  const chai = require('chai');
+  const expect = chai.expect;
+  const assert = chai.assert;
+
+  // tests
+  describe('Home component', function() {
+    context('When The app is Bootstaped', function() {
+      expect(true).to.be.true;
+    });
+  });
+})();
